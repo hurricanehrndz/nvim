d23dc8267d1a318371cc2274c645de3233fdb4fd
Carlos Hernandez <carlos@techbyte.ca>
2020-09-24 22:26:01 -0600

    Add terminal drawer keybinds, fix quotes

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,3 +1,11 @@
+local api = vim.api
 -- set leader key to space
-vim.g.mapleader = ' '
-vim.b.mapleader = ' '
+vim.g.mapleader = " "
+vim.b.mapleader = " "
+
+-- Toggle Terminal (Drawer)
+termdrawer = require("lib.termdrawer")
+api.nvim_set_keymap("n", "<leader>kh", "<cmd>lua termdrawer.toggle(1)<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<leader>kj", "<cmd>lua termdrawer.toggle(2)<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<leader>kk", "<cmd>lua termdrawer.toggle(2)<CR>", {noremap = true})
+api.nvim_set_keymap("n", "<leader>kl", "<cmd>lua termdrawer.toggle(2)<CR>", {noremap = true})
