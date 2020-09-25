14fc163b9ddf73041685ffc477453761d19a3ab8
Carlos Hernandez <carlos@techbyte.ca>
2020-09-24 22:31:17 -0600

    fix lint issues: quotes

--- lua/lib/augroups.lua
+++ lua/lib/augroups.lua
@@ -1,13 +1,13 @@
 -- util function to define autocmd groups
-af = require('lib/autofunc')
+local af = require("lib.autofunc")
 
 return function(definitions)
   for group_name, definition in pairs(definitions) do
-    vim.cmd('augroup ' .. group_name)
-    vim.cmd('autocmd!')
+    vim.cmd("augroup " .. group_name)
+    vim.cmd("autocmd!")
     for _, def in ipairs(definition) do
       af(def[1], def[2], def[3])
     end
-    vim.cmd('augroup END')
+    vim.cmd("augroup END")
   end
 end
