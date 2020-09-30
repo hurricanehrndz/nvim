508bd2d1913fefe39c34df7b1fc0abfb6e657405
Carlos Hernandez <carlos@techbyte.ca>
2020-09-29 20:37:37 -0600

    Adhere to luarocks style guide

--- lua/plugs.lua
+++ lua/plugs.lua
@@ -1,6 +1,6 @@
 local fn = vim.fn
 
-fn['plug#begin'](fn.stdpath('data') .. '/plugged')
+fn["plug#begin"](fn.stdpath('data') .. "/plugged")
 
 -- completion
 vim.cmd("Plug 'nvim-lua/completion-nvim'")
@@ -12,8 +12,8 @@ vim.cmd("Plug 'vim-airline/vim-airline-themes'")
 vim.cmd("Plug 'vim-airline/vim-airline-themes'")
 vim.cmd("Plug 'ryanoasis/vim-devicons'")
 
-fn['plug#end']()
+fn["plug#end"]()
 
 -- import plugs settings
-require('plugs.theme')
+require("plugs.theme")
 require("plugs.airline")
