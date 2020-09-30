64ddc78f77c630a884dca77dfa8436f527430812
Carlos Hernandez <carlos@techbyte.ca>
2020-09-29 20:35:15 -0600

    Add airline and devicons

--- lua/plugs.lua
+++ lua/plugs.lua
@@ -4,10 +4,16 @@ fn['plug#begin'](fn.stdpath('data') .. '/plugged')
 
 -- completion
 vim.cmd("Plug 'nvim-lua/completion-nvim'")
--- theme
+
+-- theme and look
 vim.cmd("Plug 'mhartington/oceanic-next'")
+vim.cmd("Plug 'vim-airline/vim-airline'")
+vim.cmd("Plug 'vim-airline/vim-airline-themes'")
+vim.cmd("Plug 'vim-airline/vim-airline-themes'")
+vim.cmd("Plug 'ryanoasis/vim-devicons'")
 
 fn['plug#end']()
 
 -- import plugs settings
 require('plugs.theme')
+require("plugs.airline")
