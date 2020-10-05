23b3b6ae84e10fd4ad872793b3355f2ceab845a6
Carlos Hernandez <carlos@techbyte.ca>
2020-10-04 22:29:39 -0600

    Enable lsp for javascript file

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,6 +4,9 @@ local autocmds = {
   open_terminal = {
     {"TermOpen",    "*",   [[setlocal norelativenumber | setlocal nonumber]]},
     {"TermOpen",    "*",   [[tnoremap <buffer> <Esc> <C-\><C-n>]]}
+  },
+  js_ls = {
+    {"FileType",    "javascript", [[setlocal omnifunc=v:lua.vim.lsp.omnifunc]]}
   }
 }
 
