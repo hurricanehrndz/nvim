ef58b4576ecca4118715e6897306ed5e78e22adc
Carlos Hernandez <carlos@techbyte.ca>
2020-10-05 19:33:57 -0600

    Rename augroup for js files

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -5,7 +5,7 @@ local autocmds = {
     {"TermOpen",    "*",   [[setlocal norelativenumber | setlocal nonumber]]},
     {"TermOpen",    "*",   [[tnoremap <buffer> <Esc> <C-\><C-n>]]}
   },
-  js_ls = {
+  ft_js = {
     {"FileType",    "javascript", [[setlocal omnifunc=v:lua.vim.lsp.omnifunc]]}
   }
 }
