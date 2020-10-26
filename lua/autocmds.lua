0cc63fe6eafcdca927b2f15c4eb41d555fb04176
Carlos Hernandez <carlos@techbyte.ca>
2020-10-25 21:31:46 -0600

    Exit help files use q or <c-c>

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,6 +4,10 @@ local autocmds = {
   open_terminal = {
     {"TermOpen",    "*",   [[setlocal norelativenumber | setlocal nonumber]]},
     {"TermOpen",    "*",   [[tnoremap <buffer> <Esc> <C-\><C-n>]]}
+  },
+  help_files = {
+    {"Filetype", "help", [[noremap <buffer> <silent> <C-c> :q<cr>]]},
+    {"Filetype", "help", [[noremap <buffer> <silent> q :q<cr>]]}
   }
 }
 
