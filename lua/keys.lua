ca7efa3f1b06cbb309654e6c8eac7cd930bad348
Carlos Hernandez <carlos@techbyte.ca>
2020-11-01 12:15:41 -0700

    Fix alignment in keys.lua

--- lua/keys.lua
+++ lua/keys.lua
@@ -7,21 +7,21 @@ vim.b.mapleader = ","
 api.nvim_set_keymap("i", "jk", "<ESC>", {noremap = true})
 
 -- Ctrl+[hjkl] navigate cursor in insert or command mode
-api.nvim_set_keymap("i", "<C-h>", "<Left>", {noremap = true})
-api.nvim_set_keymap("c", "<C-h>", "<Left>", {noremap = true})
-api.nvim_set_keymap("i", "<C-j>", "<Down>", {noremap = true})
-api.nvim_set_keymap("c", "<C-j>", "<Down>", {noremap = true})
-api.nvim_set_keymap("i", "<C-k>", "<Up>", {noremap = true})
-api.nvim_set_keymap("c", "<C-k>", "<Up>", {noremap = true})
-api.nvim_set_keymap("i", "<C-l>", "<Right>", {noremap = true})
-api.nvim_set_keymap("c", "<C-l>", "<Right>", {noremap = true})
+api.nvim_set_keymap("i",  "<C-h>",  "<Left>",   {noremap = true})
+api.nvim_set_keymap("c",  "<C-h>",  "<Left>",   {noremap = true})
+api.nvim_set_keymap("i",  "<C-j>",  "<Down>",   {noremap = true})
+api.nvim_set_keymap("c",  "<C-j>",  "<Down>",   {noremap = true})
+api.nvim_set_keymap("i",  "<C-k>",  "<Up>",     {noremap = true})
+api.nvim_set_keymap("c",  "<C-k>",  "<Up>",     {noremap = true})
+api.nvim_set_keymap("i",  "<C-l>",  "<Right>",  {noremap = true})
+api.nvim_set_keymap("c",  "<C-l>",  "<Right>",  {noremap = true})
 
 -- save with zz
-api.nvim_set_keymap("n", "zz", "<cmd>update<cr>", {noremap = true})
-api.nvim_set_keymap("n", "<space>zz", "<cmd>SudaWrite<cr>", {noremap = true})
+api.nvim_set_keymap("n",  "zz",         "<cmd>update<cr>",     {noremap = true})
+api.nvim_set_keymap("n",  "<space>zz",  "<cmd>SudaWrite<cr>",  {noremap = true})
 
 -- keybind disable hightlights
 api.nvim_set_keymap("n", "<leader>l", "<cmd>nohlsearch<cr>", {noremap = true})
 
 -- delete buffer
-api.nvim_set_keymap("n", "<space>bd", "<cmd>bd!<cr>", {noremap = true})
+api.nvim_set_keymap("n",  "<space>bd",  "<cmd>bd!<cr>",  {noremap = true})
