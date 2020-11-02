5fa422b44b20ea80c32d5238e7d6de9f4258b721
Carlos Hernandez <carlos@techbyte.ca>
2020-11-02 06:12:23 -0700

    Better table formatting in au file

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,11 +3,11 @@ local create_augroups = require("lib.augroups")
 local autocmds = {
   open_terminal = {
     {"TermOpen",    "*",   [[setlocal norelativenumber | setlocal nonumber]]},
-    {"TermOpen",    "*",   [[tnoremap <buffer> <Esc> <C-\><C-n>]]}
+    {"TermOpen",    "*",   [[tnoremap <buffer> <Esc> <C-\><C-n>]]},
   },
   help_files = {
     {"Filetype", "help", [[noremap <buffer> <silent> <C-c> :q<cr>]]},
-    {"Filetype", "help", [[noremap <buffer> <silent> q :q<cr>]]}
+    {"Filetype", "help", [[noremap <buffer> <silent> q :q<cr>]]},
   },
   spell_files = {
     {"Filetype", "markdown", [[setl spell spl=en]]},
